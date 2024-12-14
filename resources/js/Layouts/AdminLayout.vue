d6a023aecf42424d226b79da774ea1dcb5de5cd2
Straviux <arvin.heredero@gmail.com>
2024-12-14 23:11:16 +0800

    ....

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -27,13 +27,15 @@ import {
 // import { CogIcon } from "@heroicons/vue/20/solid";
 // import { FingerPrintIcon } from "@heroicons/vue/20/solid";
 // import { ArrowRightStartOnRectangleIcon } from "@heroicons/vue/20/solid";
+const toggleMenu = ref(false);
 </script>
 
 <template>
     <div class="w-full h-full">
         <!-- component -->
         <aside
-            class="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-[#2C3639] transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
+            class="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-[#2C3639] transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]"
+            @click="toggleMenu != toggleMenu">
             <div>
                 <div class="-mx-6 px-6 py-4">
                     <a href="#" title="home" class="text-2xl font-bold font-mono text-white">
@@ -124,14 +126,20 @@ import {
                         </svg>
                     </button>
                     <div class="flex space-x-4">
-                        <button aria-label="notification"
+                        <!-- <button aria-label="notification"
                             class="w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 m-auto text-gray-600"
                                 viewBox="0 0 20 20" fill="currentColor">
                                 <path
                                     d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
                             </svg>
-                        </button>
+                        </button> -->
+                        <Link
+                            class="px-2 py-3 flex items-center space-x-2 rounded-md text-gray-100 group hover:text-red-400"
+                            :href="route('logout')" method="post" as="button">
+                        <ArrowRightStartOnRectangleIcon class="h-4 w-4" />
+                        <span>Logout</span>
+                        </Link>
                     </div>
                 </div>
             </div>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,9 +1,9 @@
 <script setup>
 import { computed } from 'vue'
-import { Head } from "@inertiajs/vue3";
+import { Head, Link } from "@inertiajs/vue3";
 import { usePage } from '@inertiajs/vue3'
 import AdminLayout from "@/Layouts/AdminLayout.vue";
-
+import { FingerPrintIcon, IdentificationIcon, Squares2X2Icon, UserGroupIcon, UserPlusIcon } from "@heroicons/vue/20/solid";
 
 const page = usePage()
 const localip = computed(() => page.props.localip)
@@ -16,16 +16,105 @@ const localip = computed(() => page.props.localip)
     <AdminLayout>
         <template #header>Dashboard</template>
         <div class="py-4">
-            <!-- <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">
-                        Welcome {{ $page.props.auth.user.username }}
+            <div class="m-6">
+
+                <h1>Welcome {{ $page.props.auth.user.name }}</h1>
+                <div class="flex">You can access this site on your local network thru
+                    <pre class="font-semibold"> <span class="underline">{{ localip }}:9000</span></pre>
+                </div>
+
+                <div class="flex flex-wrap -mx-6 mt-6">
+                    <div class="w-full mt-6 mb-8 px-6">
+                        <p class="text-xl text-gray-600 font-semibold">Stats</p>
+                    </div>
+
+                    <div class="w-full px-6 sm:w-1/2 xl:w-1/3">
+                        <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-slate-100">
+                            <div class="p-3 rounded-full bg-indigo-600 bg-opacity-75">
+                                <UserGroupIcon class="h-8 w-8 text-white" />
+                            </div>
+
+                            <div class="mx-5">
+                                <h4 class="text-2xl font-semibold text-gray-700">4644</h4>
+                                <div class="text-gray-500">Total Voters</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="w-full mt-6 px-6 sm:w-1/2 xl:w-1/3 sm:mt-0">
+                        <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-slate-100">
+                            <div class="p-3 rounded-full bg-orange-600 bg-opacity-75">
+
+                                <IdentificationIcon class="h-8 w-8 text-white" />
+                            </div>
+
+                            <div class="mx-5">
+                                <h4 class="text-2xl font-semibold text-gray-700">3453</h4>
+                                <div class="text-gray-500">Total Profiles</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="w-full mt-6 px-6 sm:w-1/2 xl:w-1/3 xl:mt-0">
+                        <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-slate-100">
+                            <div class="p-3 rounded-full bg-pink-600 bg-opacity-75">
+                                <UserPlusIcon class="h-8 w-8 text-white" />
+                            </div>
+
+                            <div class="mx-5">
+                                <h4 class="text-2xl font-semibold text-gray-700">678</h4>
+                                <div class="text-gray-500">New Profile</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div> -->
-            <h1>Welcome {{ $page.props.auth.user.name }}</h1>
-            <div class="flex">You can access this site on your local network thru
-                <pre class="font-semibold"> <span class="underline">{{ localip }}:9000</span></pre>
+
+                <div class="flex flex-wrap -mx-6 mt-8 px-6 gap-x-16">
+                    <div class="w-full mt-6 mb-8">
+                        <p class="text-xl text-gray-600 font-semibold">Links</p>
+                    </div>
+
+
+                    <Link :href="route('dashboard')"
+                        class="flex items-center py-6 rounded-lg bg-white shadow border border-gray-300 transition hover:-translate-y-2 duration-150 hover:shadow-lg w-1/2 mt-6 px-6 sm:w-1/4 md:w-64 lg:w-64 xl:w-64 xl:mt-0">
+
+
+                    <Squares2X2Icon class="h-8 w-8 text-slate-600" />
+
+
+                    <div class="mx-3">
+                        <h4 class="text-xl underline underline-offset-4 font-semibold text-gray-600">Dashboard</h4>
+                    </div>
+
+                    </Link>
+
+                    <Link :href="route('voterslist.index')"
+                        class="flex items-center py-6 rounded-lg bg-white shadow border border-gray-300 transition hover:-translate-y-2 duration-150 hover:shadow-lg w-1/2 mt-6 px-6 sm:w-1/4 md:w-64 lg:w-64 xl:w-64 xl:mt-0">
+
+
+                    <FingerPrintIcon class="h-8 w-8 text-slate-600" />
+
+
+                    <div class="mx-3">
+                        <h4 class="text-xl underline underline-offset-4 font-semibold text-gray-600">Voters List</h4>
+                    </div>
+
+                    </Link>
+
+                    <Link :href="route('votersprofile.index')"
+                        class="flex items-center py-6 rounded-lg bg-white shadow border border-gray-300 transition hover:-translate-y-2 duration-150 hover:shadow-lg w-1/2 mt-6 px-6 sm:w-1/4 md:w-64 lg:w-64 xl:w-64 xl:mt-0">
+
+
+                    <UserGroupIcon class="h-8 w-8 text-slate-600" />
+
+
+                    <div class="mx-3">
+                        <h4 class="text-xl underline underline-offset-4 font-semibold text-gray-600">Kiel's Profiling
+                        </h4>
+                    </div>
+
+                    </Link>
+                </div>
             </div>
         </div>
     </AdminLayout>
